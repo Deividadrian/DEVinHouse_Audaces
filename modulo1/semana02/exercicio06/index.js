23ca8eb0569dd9e6089e9f64ef846d286da52515
deividadrian <deividadrian24@gmail.com>
2022-12-09 19:52:51 -0300

    Exercício06 da semana02

---
+++ modulo1/semana02/exercicio06/index.js
@@ -0,0 +1,17 @@
+const operador = prompt(
+  'Digite com qual operação deseja realizar (+) (-) (*) (/)'
+)
+const numero1 = parseInt(prompt('Digite o primeiro número'))
+const numero2 = parseInt(prompt('Digite o segundo número'))
+
+if (operador === '+') {
+  alert(`O resultado do seu cálculo é: ${numero1 + numero2}`)
+} else if (operador === '-') {
+  alert(`O resultado do seu cálculo é: ${numero1 - numero2}`)
+} else if (operador === '*') {
+  alert(`O resultado do seu cálculo é: ${numero1 * numero2}`)
+} else if (operador === '/') {
+  alert(`O resultado do seu cálculo é: ${numero1 / numero2}`)
+} else if (!operador || !numero1 || !numero2) {
+  alert(`Algum campo não foi digitado!`)
+}
